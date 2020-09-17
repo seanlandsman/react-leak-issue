21eacb8e84ecf7257368f55312d10b2ece2a8a59
Sean Landsman <sean@thelandsmans.com>
2020-09-17 13:12:32 +0100

    update

---
+++ build/precache-manifest.43467efa016b50117733296acd5465ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e65652ca60e951d7086c281fba1c3e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c231ea624e4906e42d3",
+    "url": "/static/css/main.2a8dc532.chunk.css"
+  },
+  {
+    "revision": "9bda7540ae22a28c8bd0",
+    "url": "/static/js/2.b86e411b.chunk.js"
+  },
+  {
+    "revision": "2c598234c7a24970b5fdf032f602e7bc",
+    "url": "/static/js/2.b86e411b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c231ea624e4906e42d3",
+    "url": "/static/js/main.42757ad5.chunk.js"
+  },
+  {
+    "revision": "d1edc1ec42abbbca52717ebb41c69c41",
+    "url": "/static/js/main.42757ad5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0255ff7d238c482ca13a",
+    "url": "/static/js/runtime-main.6ede62d1.js"
+  }
+]);
